7a7d7cbdac318af087e46d27447088cb2f13f917
Abdullah Abutayyem <51279171+abdullah-tym@users.noreply.github.com>
2026-06-03 21:40:15 +0300

    Update 01_data_cleaning_localization.sql

--- 01_data_cleaning_localization.sql
+++ 01_data_cleaning_localization.sql
@@ -1,1 +1,25 @@
+ALTER TABLE public.superstore2 
+    ALTER COLUMN sales TYPE NUMERIC USING sales::numeric,
+    ALTER COLUMN profit TYPE NUMERIC USING profit::numeric,
+    ALTER COLUMN discount TYPE NUMERIC USING discount::numeric,
+    ALTER COLUMN quantity TYPE INT USING quantity::integer,
+    ALTER COLUMN order_date TYPE DATE USING order_date::date,
+    ALTER COLUMN ship_date TYPE DATE USING ship_date::date;
 
+UPDATE public.superstore2
+SET 
+    country = 'Saudi Arabia',
+    region = CASE 
+        WHEN region = 'West' THEN 'Western Province'
+        WHEN region = 'East' THEN 'Eastern Province'
+        WHEN region = 'Central' THEN 'Central Province'
+        ELSE 'Southern Province'
+    END,
+    state = CASE 
+        WHEN state IN ('California', 'Washington', 'Oregon') THEN 'Makkah Province'
+        WHEN state IN ('New York', 'Pennsylvania', 'Ohio') THEN 'Riyadh Province'
+        WHEN state IN ('Texas', 'Illinois', 'Florida') THEN 'Eastern Province'
+        ELSE 'Medina Province'
+    END,
+    sales = ROUND(sales * 3.75, 2),
+    profit = ROUND(profit * 3.75, 2);
